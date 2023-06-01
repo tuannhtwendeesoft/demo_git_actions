a3a2986987f7e714b38a05f9ce30b7b1b81d8ad6
tuannh <tuannh@twendeesoft.com>
2023-06-01 15:02:01 +0700

    update code

--- src/main/java/com/example/demo_git_actions/CheckController.java
+++ src/main/java/com/example/demo_git_actions/CheckController.java
@@ -1,12 +1,16 @@
 package com.example.demo_git_actions;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CheckController {
+    @Value("${redis.port}")
+    private String texxt;
+
     @GetMapping(value = "health-check")
     public String healCheck() {
-        return "check check check";
+        return "check check check hello kitty" + texxt;
     }
 }
